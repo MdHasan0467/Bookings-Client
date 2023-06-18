8ea8448f530182e2876ddfddd60b93b88b3e0100
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-06-18 11:55:43 +0600

    home page designed

---
+++ src/Components/Home/QuickAndEasyTripPlanner.jsx
@@ -0,0 +1,12 @@
+
+
+const QuickAndEasyTripPlanner = () => {
+    return (
+        <div>
+            <h1 className="ml-5 lg:ml-16 text-2xl font-semibold">Quick and easy trip planner</h1>
+            <p className="ml-5 lg:ml-16 mb-3">Pick a vibe and explore the top destinations in Bangladesh</p>
+        </div>
+    );
+};
+
+export default QuickAndEasyTripPlanner;
